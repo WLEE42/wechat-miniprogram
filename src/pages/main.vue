b466debfdd3ccd302ff6f7645b581dff9b77b36b
lwq <xraylwq@gmail.com>
2018-11-24 21:07:03 +0800

    修改main.vue

---
+++ src/pages/main.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {}
+  },
+
+  components: {
+  },
+
+  computed: {
+  },
+
+  mounted () {
+    this.showTodos()
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
