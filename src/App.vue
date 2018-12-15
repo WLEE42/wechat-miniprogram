ab068a8a5918adc54bbd19caa99836320da2f4cd
lwq <xraylwq@gmail.com>
2018-12-16 00:10:56 +0800

    添加了测试文件，去掉了pug

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
           // 发送凭证
           // 发起网络请求
           wx.request({
-            url: 'http://39.96.33.101:80/login',
+            url: 'http://39.96.33.101:80/login/',
             data: {
               code: res.code
             },
